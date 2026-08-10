6d62536b1936505ebd4f95ed087e435ae6535123
Danileros <>
2026-08-10 13:16:49 +0300

    debug stub

---
+++ Managers/DebugManagerProxy.cs
@@ -0,0 +1,16 @@
+namespace LongLargo.Managers;
+
+public class DebugManagerProxy
+{
+    public DebugManagerProxy()
+    {
+    }
+    
+    public void RegisterDebugCommand(string commandName, Func<string> debugOutputGetter)
+    {
+    }
+
+    public void UnregisterDebugCommand(string commandName)
+    {
+    }
+}
